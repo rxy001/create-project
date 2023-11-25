f0656ee9cd0f4be0072d00dba593881e3d9d0360
renxingyu <handsome.rxy@gmail.com>
2023-11-25 16:32:49 +0800

    fix: update config

--- template-lib/rollup.config.ts
+++ template-lib/rollup.config.ts
@@ -5,22 +5,30 @@ import { defineConfig } from "rollup"
 import { nodeResolve } from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 import terser from "@rollup/plugin-terser"
+import dts from "rollup-plugin-dts"
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url))
 
 export default () =>
-  defineConfig({
-    input: "src/app.ts",
-    output: {
-      dir: "build",
-      format: "es",
+  defineConfig([
+    {
+      input: "src/app.ts",
+      output: {
+        format: "es",
+        file: "build/index.js",
+      },
+      plugins: [
+        typescript({
+          tsconfig: path.resolve(__dirname, "tsconfig.json"),
+        }),
+        nodeResolve(),
+        commonjs(),
+        terser(),
+      ],
     },
-    plugins: [
-      typescript({
-        tsconfig: path.resolve(__dirname, "tsconfig.json"),
-      }),
-      nodeResolve(),
-      commonjs(),
-      terser()
-    ],
-  })
+    {
+      input: "./build/types/src/app.d.ts",
+      output: [{ file: "build/index.d.ts", format: "es" }],
+      plugins: [dts()],
+    },
+  ])
